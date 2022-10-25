f135726615468a783bbe9a578f2b7e12983102ec
Marina <mvtravell@mail.ru>
2022-10-25 15:04:28 +0300

    Sprint6_final_project_new_new

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -10,28 +10,20 @@ using namespace std;
 void RemoveDuplicates(SearchServer& search_server) {
     map<set<string>, int> id_documents_with_words; //в словаре автоматически останутся только недублированные наборы слов и id документов
     set<string> set_of_words; //собираем наборы слов - ключи словаря
-
+    set<int> id_duplicates; //собираем id дубликатов
     for (const int document_id : search_server) {
         map<string, double> word_freq = search_server.GetWordFrequencies(document_id);
         for (const auto& [word, tf] : word_freq) {
             set_of_words.insert(word);
         }
-        id_documents_with_words.insert({set_of_words, document_id});
+        if (id_documents_with_words.count(set_of_words) == 0) {
+            id_documents_with_words.insert({set_of_words, document_id});
+        } else {
+            id_duplicates.insert(document_id);
+            }
         set_of_words.clear();
     }
-
-    set<int> id_without_duplicates; //собираем id оригинальных документов
-    for (const auto [words, id] : id_documents_with_words) {
-        id_without_duplicates.insert(id);
-    }
-
-    set<int> id_duplicates; //собираем id дубликатов
-    for (const int document_id : search_server) {
-        if (!id_without_duplicates.count(document_id)) {
-            id_duplicates.insert(document_id);
-        }
-    }
-
+  
     for (const auto& id : id_duplicates) {
         cout << "Found duplicate document id "s << id << endl;
         search_server.RemoveDocument(id);
